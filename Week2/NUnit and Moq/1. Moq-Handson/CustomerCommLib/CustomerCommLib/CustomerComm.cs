205556a9619036a7a2b3b054e165323296c74c80
Sourav <souravparida170@gmail.com>
2025-06-28 11:19:36 +0530

    updated week2

---
+++ Week2/NUnit and Moq/1. Moq-Handson/CustomerCommLib/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            string address = "cust123@abc.com";
+            string message = "Some Message";
+
+            _mailSender.SendMail(address, message);
+
+            return true;
+        }
+    }
+}
